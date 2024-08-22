53dd56ee7e32bd3d60fa7721a7990b5938823c16
Daniil Telegin <daniil.collis@gmail.com>
2024-08-22 15:06:00 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from random import random
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def random_code():
+    return random.randint(1000,9999)
+
+if __name__ == '__main__':
+    app.run(host=0.0.0.0, port=5000)
